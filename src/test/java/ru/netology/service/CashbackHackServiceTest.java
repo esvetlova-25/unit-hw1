68d522303844fbab6c8ed7f088a13551e9c1ec6e
Kate test <d.swetlov2017@gmail.com>
2024-02-22 20:43:36 +0300

    new branch

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,29 +1,30 @@
 package ru.netology.service;
 
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
 
-    public void shouldEarnCashback(){
+    public void shouldEarnCashback() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
 
         int actual = service.remain(amount);
         int expected = 0;
-        
-        assertEquals(expected, actual);
+
+        assertEquals(actual, expected);
     }
-    @org.testng.annotations.Test
-    public void shouldNotEarnCashback(){
+
+    @org.junit.Test
+    public void shouldNotEarnCashback() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
 
         int actual = service.remain(amount);
         int expected = 100;
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 }
